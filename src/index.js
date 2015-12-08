f4b0069798b1dd2bc751a76e87ef3a7234af8f26
Piet Geursen <pietgeursen@gmail.com>
2015-12-08 21:17:07 +1300

    Start piping

--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ var split = require('split2')
 // let game = startGame()
 
 process.stdin
-  .pipe(split('\n'))
+  .pipe(split())
   .pipe(through(function (chunk, enc, cb) {
-    console.log(chunk)
-    this.push(chunk)
+    cb(null, chunk.toString().toUpperCase()) 
   }))
+  .pipe(through.obj(function (chunk, enc, cb) {
+    cb(null, {wee: 'wooo'})
+  }))
+  .on('data', function(data){
+    console.log(data);  
+  })
+  //.pipe(process.stdout)
